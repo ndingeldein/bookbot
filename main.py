0458b53ab07c136c4cf9a52829718d741b7e8364
neil dingeldein <ndingeldein@gmail.com>
2024-04-11 18:07:38 -0500

    chapter 16 print report

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ def main():
     book_text = get_book_text(book_path)
     words_count = get_words(book_text)
     characters = get_characters(book_text)
-    print(f"{book_title} has {words_count} words and the following characters:")
-    print(characters)
+
+    print(f"--- Begin report of {book_path} ---\n")
+    print(f"{words_count} words found in {book_title}")
+    for character in get_character_list(characters):
+        print(f"The {character['char']} character appears {character['count']} times")
+
+    print(f"\n--- End report of {book_path} ---")
 
 def get_title(path):
     return path.split("/")[-1].split(".")[0]
@@ -16,12 +21,26 @@ def get_words(txt):
 def get_characters(txt):
     characters = {}
     for char in txt:
-        char = char.lower()
-        if char in characters:
-            characters[char] += 1
-        else:
-            characters[char] = 1
+        if char.isalpha():
+            lowercased = char.lower()
+            if lowercased in characters:
+                characters[lowercased] += 1
+            else:
+                characters[lowercased] = 1
     return characters
+
+def get_character_list(characters):
+    character_list = []
+    for char in characters:
+        character_list.append({
+            "char": char,
+            "count": characters[char]
+        })
+    character_list.sort(reverse=True, key=sort_characters)
+    return character_list
+
+def sort_characters(characters):
+    return characters["count"]
 
 def get_book_text(path):
     with open(path) as f:
